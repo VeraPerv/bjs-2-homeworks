039a7fce0b91454d6b01b1bbb36977e5b257f3fb
Vera Pervushina <vera.pervushina2608@gmail.com>
2022-08-30 18:33:30 +0700

    Initial commit

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,220 @@
 // Задание 1
 function getArrayParams(arr) {
   let min, max, sum, avg;
+  sum = 0;
+ // let index = 0;
+  min = arr[0];
+  max = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  avg = (sum / arr.length);
 
-  // Ваш код
-
-  return { min: min, max: max, avg: avg };
+  return {
+    min: min,
+    max: max,
+    avg: Number(avg.toFixed(2))
+  };
 }
+getArrayParams([1, 2, 3, -100, 10]);
 
 // Задание 2
+
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
   return sum;
 }
+worker([1, 2, 3, 4]);
 
 function makeWork(arrOfArr, func) {
+ // let index = 0;
   let max;
-
-  // Ваш кода
-  // for ...
-  
-  return max;
+  let sum;
+  let arrDifference;
+  for (let j = 1; j < arrOfArr.length; j++) {
+    max = func(arrOfArr[0]);
+    sum = func(arrOfArr[j]);
+    arrDifference = func(arrOfArr[j]);
+    if (sum > max) {
+      max = sum;
+      //debugger;
+      return max;
+    }
+    if (arrDifference > max) {
+      max = arrDifference;
+      return Math.abs(max - arrDifference);
+   
+    //Math.abs(max - arrDifference);
+    
+  }
 }
+}
+makeWork([
+  [10, 10, 11],
+  [20, 10]
+], worker);
+makeWork([
+  [-10, -20, -40],
+  [10, 20, 30]
+], worker2);
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  //let index = 0;
+  let max = arr[0];
+  let min = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+ // let difference = Math.abs(max) - Math.abs(min);
+  //return Math.abs(difference);
+  return Math.abs(max - min);
 }
+
+worker2([-10, -2, 3, 4]);
+
+/*  let min, max, sum, avg;
+  sum = 0;
+  let index = 0;
+  min = arr[index];
+
+  for (let i = index + 1; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+
+  max = arr[index];
+  for (let i = index + 1; i < arr.length; i++) {
+
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  avg = (sum / arr.length);
+
+  return {
+    min: min,
+    max: max,
+    avg: Number(avg.toFixed(2))
+  };
+}
+getArrayParams([1, 2, 3, -100, 10]);*/
+
+/* function getArrayParams(arr) {
+  let min, max, sum, avg;
+  sum = 0;
+ // let index = 0;
+  min = arr[0];
+  max = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  avg = (sum / arr.length);
+
+  return {
+    min: min,
+    max: max,
+    avg: Number(avg.toFixed(2))
+  };
+}
+getArrayParams([1, 2, 3, -100, 10]);
+
+// Задание 2
+
+function worker(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+}
+worker([1, 2, 3, 4]);
+
+function makeWork(arrOfArr, func) {
+ // let index = 0;
+  let max;
+  let sum;
+  let arrDifference;
+  for (let j = 1; j < arrOfArr.length; j++) {
+    max = func(arrOfArr[0]);
+    sum = func(arrOfArr[j]);
+    arrDifference = func(arrOfArr[j]);
+    if (sum > max) {
+      max = sum;
+      return max;
+    } else {
+      return max;
+    }
+    if (arrDifference > max) {
+      max = arrDifference;
+      return Math.abs(max) - Math.abs(arrDifference);
+    } else {
+      return Math.abs(max) - Math.abs(arrDifference);
+    }
+
+  }
+}
+makeWork([
+  [10, 10, 11],
+  [20, 10]
+], worker);
+makeWork([
+  [-10, -20, -40],
+  [10, 20, 30]
+], worker2);
+
+// Задание 3
+function worker2(arr) {
+  //let index = 0;
+  let max = arr[0];
+  let min = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+ // let difference = Math.abs(max) - Math.abs(min);
+  //return Math.abs(difference);
+  return Math.abs(max - min);
+}
+
+worker2([-10, -2, 3, 4]);
+*/
--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,55 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
+
+let firstStudent = new Student('Anton', 'm', 19);
+let secondStudent = new Student('Anna', 'f', 18);
+let thirdStudent = new Student('Fedor', 'm', 22);
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
-}
+  this.subject = subjectName; //ваш код
+};
+firstStudent.setSubject('algebra');
 
 // ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  // this.marks = [];
+  if (this.marks === undefined) {
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark);
+  }
+  //}
+};
+firstStudent.addMark(5);
+secondStudent.addMark(4);
+console.log(secondStudent);
+
+
+Student.prototype.addMarks = function (...marks) {
+  //this.marks = [];
+  this.marks = marks;
+};
+thirdStudent.addMarks(1, 2, 3, 4, 5);
+firstStudent.addMarks(5, 5, 5);
+console.log(firstStudent);
+
+
+Student.prototype.getAverage = function (...array) {
+  this.average = (array.reduce((acc, item) => acc += item, 0)) / array.length;
+};
+//console.log(marks)
+firstStudent.getAverage(this.marks);
+console.log(firstStudent.marks);
+console.log(firstStudent);
+
+
+Student.prototype.exclude = function (reason) {
+  this.excluded = reason;
+  delete this.marks;
+  delete this.subject;
+};
+
+firstStudent.exclude('low grades');
